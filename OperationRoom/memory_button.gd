15e3d5e13a40df10b67e8b166db5aab741f33316
Ghislain Thiery <ghislain.thiery@ntymail.com>
2025-02-16 10:35:08 +0100

    added the contextual menu on memory_button

--- OperationRoom/memory_button.gd
+++ OperationRoom/memory_button.gd
@@ -2,30 +2,50 @@ class_name MemoryButton extends TextureButton
 
 @onready var title_label: Label = $MemoryLabel
 @onready var cost_label: Label = $CostLabel
+@onready var context_menu: Control = $ContextualMenu
+@onready var context_title: Label = $ContextualMenu/MarginContainer/VBoxContainer/ContextTitleLabel
+@onready var context_description: RichTextLabel = $ContextualMenu/MarginContainer/VBoxContainer/RichTextLabel
 @onready var on_ready_memory: MemoryData
+@onready var keep_button: Button = $ContextualMenu/MarginContainer/VBoxContainer/KeepButton
+@onready var insert_button: Button = $ContextualMenu/MarginContainer/VBoxContainer/InsertButton
+@onready var sell_button: Button = $ContextualMenu/MarginContainer/VBoxContainer/SellButton
 
 #region Methods to display memory buttons
 #...in the shop
 func DisplayInShop(memory: MemoryData):
 	Update(memory)
 	cost_label.show()
+	keep_button.hide()
+	insert_button.show()
 
 #...in the memory eraser
 func DisplayInEraser(memory: MemoryData):
 	Update(memory)
 	cost_label.hide()
+	self.disabled = !memory.can_be_clicked
+	keep_button.show()
+	insert_button.hide()
 #endregion
 
 func Update(memory: MemoryData):
+	on_ready_memory = memory
+	# Title
 	title_label.text = memory.memory_title
-	cost_label.text = str(memory.memory_cost, " credits")
+	context_title.text = memory.memory_title
+	# Cost
+	cost_label.text = str("$", memory.memory_cost)
+	sell_button.text = str("Sell for $", memory.memory_cost)
+	# Description
 	tooltip_text = memory.memory_description
+	context_description.text = memory.memory_description
+	# Thumbnail
 	texture_normal = memory.memory_thumbnail
 	texture_hover = memory.memory_thumbnail
 	texture_pressed = memory.memory_thumbnail
 	texture_focused = memory.memory_thumbnail
 
 func _on_pressed():
+	context_menu.show()
 	print("a button has been pressed!")
 
 func _on_button_down():
@@ -39,3 +59,7 @@ func _on_mouse_entered():
 
 func _on_mouse_exited():
 	scale = Vector2(1, 1)
+
+
+func _on_contextual_menu_mouse_exited():
+	context_menu.hide()
