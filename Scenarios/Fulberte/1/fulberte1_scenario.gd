bf3222f4d2df143372927ff62b184144efb4d951
Ghislain Thiery <ghislain.thiery@ntymail.com>
2025-02-22 06:50:07 +0100

    scenario factory

--- Scenarios/Fulberte/1/fulberte1_scenario.gd
+++ Scenarios/Fulberte/1/fulberte1_scenario.gd
@@ -2,7 +2,8 @@ extends ScenarioBase
 class_name Fulberte1Scenario
 
 var _startLines : Array[DialogLine] = []
-var _goodEndingLines : Array[DialogLine] = []
+var _forgetFriendsLines : Array[DialogLine] = []
+var _forgetLoveLines : Array[DialogLine] = []
 var _badEndingLines : Array[DialogLine] = []
 var _noChangeLines : Array[DialogLine] = []
 
@@ -13,12 +14,26 @@ func _ready():
 	_state = Enums.ScenarioState.Opening 
 	
 	# Opening
-	_startLines.append(DialogLineFactory.CreateLine(Enums.Talker.Patient, ""))
-	_startLines.append(DialogLineFactory.CreateLine(Enums.Talker.Doctor, ""))
+	_startLines.append(DialogLineFactory.CreateLine(Enums.Talker.Patient, "Hello. I've heard that you could... *change* some of my memories?"))
+	_startLines.append(DialogLineFactory.CreateLine(Enums.Talker.Doctor, "That's right. First time?"))
+	_startLines.append(DialogLineFactory.CreateLine(Enums.Talker.Doctor, "Don't worry, we've all been there. What memory do you want to change?"))
+	_startLines.append(DialogLineFactory.CreateLine(Enums.Talker.Patient, "You see, I have been a Gaiist for all my life."))
+	_startLines.append(DialogLineFactory.CreateLine(Enums.Talker.Patient, "Trying to restore our wounded Earth with my little cult friends..."))
+	_startLines.append(DialogLineFactory.CreateLine(Enums.Talker.Patient, "But a couple of standards ago, I bumped into a young astronaut and I fell in love."))
+	_startLines.append(DialogLineFactory.CreateLine(Enums.Talker.Patient, "I ran away with them. Got a new haircut, tried smash for the first time..."))
+	_startLines.append(DialogLineFactory.CreateLine(Enums.Talker.Patient, "I had the best time of my life!"))
+	_startLines.append(DialogLineFactory.CreateLine(Enums.Talker.Patient, "But sometimes, I think about my former friends... and I regret my decision. "))
+	_startLines.append(DialogLineFactory.CreateLine(Enums.Talker.Patient, "Can you help me?"))
 
-	# Good ending
-	_goodEndingLines.append(DialogLineFactory.CreateLine(Enums.Talker.Patient, ""))
-	_goodEndingLines.append(DialogLineFactory.CreateLine(Enums.Talker.Doctor, ""))
+	# Forget her friends
+	_forgetFriendsLines.append(DialogLineFactory.CreateLine(Enums.Talker.Patient, "...I feel relieved. As if an heavy weight was gone."))
+	_forgetFriendsLines.append(DialogLineFactory.CreateLine(Enums.Talker.Patient, "Now, if you'll excuse me... Life is short, and I don't want to spend another minute away from my love."))
+	
+	# Forget her love
+	_forgetLoveLines.append(DialogLineFactory.CreateLine(Enums.Talker.Patient, "...I feel empty. What happened?"))
+	_forgetLoveLines.append(DialogLineFactory.CreateLine(Enums.Talker.Doctor, "Are you alright?"))
+	_forgetLoveLines.append(DialogLineFactory.CreateLine(Enums.Talker.Patient, "I'm not sure. I'm not *hurt*, but it's like... as if... there was a hole in my chest."))
+	_forgetLoveLines.append(DialogLineFactory.CreateLine(Enums.Talker.Patient, "I must leave now. I want to go home."))
 	
 	# Bad ending
 	_badEndingLines.append(DialogLineFactory.CreateLine(Enums.Talker.Doctor, "(Techno-dammit, I messed up!)"))
@@ -27,8 +42,12 @@ func _ready():
 	_badEndingLines.append(DialogLineFactory.CreateLine(Enums.Talker.Doctor, "(I should be more cautious from now on.)"))
 
 	# No change ending
-	_noChangeLines.append(DialogLineFactory.CreateLine(Enums.Talker.Patient, ""))
-	_noChangeLines.append(DialogLineFactory.CreateLine(Enums.Talker.Doctor, ""))
+	_noChangeLines.append(DialogLineFactory.CreateLine(Enums.Talker.Patient, "So, you choose not to erase anything. Why?"))
+	_noChangeLines.append(DialogLineFactory.CreateLine(Enums.Talker.Doctor, "I think your regrets are part of your story. You should value them, too"))
+	_noChangeLines.append(DialogLineFactory.CreateLine(Enums.Talker.Doctor, "They help you make the right choices. They make you a better person."))
+	_noChangeLines.append(DialogLineFactory.CreateLine(Enums.Talker.Patient, "Indeed..."))
+	_noChangeLines.append(DialogLineFactory.CreateLine(Enums.Talker.Patient, "..."))
+	_noChangeLines.append(DialogLineFactory.CreateLine(Enums.Talker.Patient, "...I cannot pay you for nothing, but I get it now. Thank you, doctor."))
 
 	LoadLines(_startLines)
 	
@@ -53,18 +72,31 @@ func ResolveAndCheckIfFried(souvenirs : Array[MemoryData]) -> bool:
 		LoadLines(_badEndingLines)
 		return true
 
-	var hasHappy = souvenirs[1].tags.find(Enums.MemTag.Happy) >= 0
-	if hasHappy:
-		_pay = 400
-		LoadLines(_goodEndingLines)
-	else:
-		_pay = 50
+	var loveCount = 0
+	var familyCount = 0
+	for s in souvenirs:
+		if s.tags.find(Enums.MemTag.Family) >= 0:
+			familyCount += 1
+		elif s.tags.find(Enums.MemTag.Love) >= 0:
+			loveCount +=1
+			
+	# 2. check tags
+	if souvenirs[1].tags.find(Enums.MemTag.LifePath) >= 0 and souvenirs[2].tags.find(Enums.MemTag.LifePath) >= 0:
+		_pay = 0
 		LoadLines(_noChangeLines)
+	else:
+		if loveCount < familyCount:
+			_pay = 400
+			LoadLines(_forgetLoveLines)
+		elif loveCount >= familyCount:
+			_pay = 800
+			LoadLines(_forgetFriendsLines)
 	
 	_state = Enums.ScenarioState.OperationResult
 	_completed = true
 	return isFried
 
 func _isFried(souvenirs : Array[MemoryData]) -> bool:
-	var tags = souvenirs[1].tags
-	return tags.find(Enums.MemTag.Family) < 0
+	if souvenirs[1].tags.find(Enums.MemTag.Family) < 0 and souvenirs[1].tags.find(Enums.MemTag.Love) < 0 and souvenirs[2].tags.find(Enums.MemTag.Family) < 0 and souvenirs[2].tags.find(Enums.MemTag.Love) < 0:
+			return true
+	return false
