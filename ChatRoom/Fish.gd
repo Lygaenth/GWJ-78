76104423b823422a953ead92f8dc36aa9a4f9a65
Ghislain Thiery <ghislain.thiery@ntymail.com>
2025-02-23 02:48:39 +0100

    new fishes!

--- ChatRoom/Fish.gd
+++ ChatRoom/Fish.gd
@@ -1,13 +1,17 @@
 extends Sprite2D
 class_name Fish
 
-var speed : float = 20
 @export var direction : Vector2
-var sens : float = -1
+@export var speed : float = 20
+@export var vsens : float = -1
+@export var hsens : float = -1
+
 
 func _process(delta):
-	position  = position + sens * speed * delta * direction.normalized()
-	if (position.y < 200 || position.y > 800):
-		sens *= -1
-
-	flip_h = sens > 0
+	position  = position + speed * delta * Vector2(direction.x * hsens, direction.y * vsens).normalized()
+	if (position.y < 50 || position.y > 750):
+		vsens *= -1
+	if (position.x < 50 || position.x > 200):
+		hsens *= -1
+		
+	flip_h = hsens < 0
